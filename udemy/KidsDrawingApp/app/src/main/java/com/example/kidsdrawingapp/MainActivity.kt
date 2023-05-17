fb0e99850b70db5e68275ede741b4afaf8d17559
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-17 22:13:40 +0900

    캔버스 주변에 보더 추가

--- udemy/KidsDrawingApp/app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ udemy/KidsDrawingApp/app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.kidsdrawingapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.kidsdrawingapp.ui.theme.KidsDrawingAppTheme
 
@@ -20,12 +24,13 @@ class MainActivity : ComponentActivity() {
             KidsDrawingAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize().padding(9.dp),
+                    color = Color.White
                 ) {
-                    AndroidView(factory = {
-                        DrawingView(it)
-                    })
+                    AndroidView(
+                        factory = { context -> DrawingView(context) },
+                        modifier = Modifier.border(BorderStroke(0.5.dp, Color(0xFF9AA2AF)))
+                    )
                 }
             }
         }
