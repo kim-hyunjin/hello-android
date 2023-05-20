78226676b8b1cb21c9156cf907ed6eded3afc003
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-20 16:13:59 +0900

    배경화면 추가

--- udemy/KidsDrawingApp/app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ udemy/KidsDrawingApp/app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -32,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,22 +65,37 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
     @Composable
     fun DrawingCanvas() {
         val configuration = LocalConfiguration.current
         val canvasHeight = (configuration.screenHeightDp * 0.85).dp
 
-        AndroidView(
-            factory = {
-                drawingView = DrawingView(it)
-                drawingView.setSizeForBrush(20f)
-                drawingView
-            } ,
-            modifier = Modifier
-                .border(BorderStroke(0.5.dp, Color(0xFF9AA2AF)))
-                .height(canvasHeight)
-        )
+        Box(
+            modifier = Modifier.height(canvasHeight)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.image),
+                contentDescription = "canvas background image",
+                alignment = Alignment.Center,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.height(canvasHeight)
+            )
+            AndroidView(
+                factory = {
+                    drawingView = DrawingView(it)
+                    drawingView.setSizeForBrush(20f)
+                    drawingView
+                },
+                modifier = Modifier
+                    .border(BorderStroke(0.5.dp, Color(0xFF9AA2AF)))
+                    .height(canvasHeight)
+                    .background(Color(0x80FFFFFF))
+            )
+        }
+
     }
+
     @Preview
     @Composable
     fun ToolArea() {
@@ -107,10 +124,13 @@ class MainActivity : ComponentActivity() {
                 .border(2.dp, Color(0xFF999999), RoundedCornerShape(10.dp)),
             horizontalArrangement = Arrangement.Center
         ) {
-            ColorButton(color = Color.Black, isActive = Color.Black == currentColor.value, onClick = {
-                currentColor.value = Color.Black
-                setColor(Color.Black)
-            })
+            ColorButton(
+                color = Color.Black,
+                isActive = Color.Black == currentColor.value,
+                onClick = {
+                    currentColor.value = Color.Black
+                    setColor(Color.Black)
+                })
             ColorButton(color = Color.Red, isActive = Color.Red == currentColor.value, onClick = {
                 currentColor.value = Color.Red
                 setColor(Color.Red)
@@ -119,18 +139,27 @@ class MainActivity : ComponentActivity() {
                 currentColor.value = Color.Blue
                 setColor(Color.Blue)
             })
-            ColorButton(color = Color.Yellow, isActive = Color.Yellow == currentColor.value, onClick = {
-                currentColor.value = Color.Yellow
-                setColor(Color.Yellow)
-            })
-            ColorButton(color = Color.Green, isActive = Color.Green == currentColor.value, onClick = {
-                currentColor.value = Color.Green
-                setColor(Color.Green)
-            })
-            ColorButton(color = Color.Magenta, isActive = Color.Magenta == currentColor.value, onClick = {
-                currentColor.value = Color.Magenta
-                setColor(Color.Magenta)
-            })
+            ColorButton(
+                color = Color.Yellow,
+                isActive = Color.Yellow == currentColor.value,
+                onClick = {
+                    currentColor.value = Color.Yellow
+                    setColor(Color.Yellow)
+                })
+            ColorButton(
+                color = Color.Green,
+                isActive = Color.Green == currentColor.value,
+                onClick = {
+                    currentColor.value = Color.Green
+                    setColor(Color.Green)
+                })
+            ColorButton(
+                color = Color.Magenta,
+                isActive = Color.Magenta == currentColor.value,
+                onClick = {
+                    currentColor.value = Color.Magenta
+                    setColor(Color.Magenta)
+                })
         }
     }
 
@@ -138,33 +167,37 @@ class MainActivity : ComponentActivity() {
         val colorIntValue = color.toArgb()
         drawingView.setColorForBrush(colorIntValue)
     }
+
     @Composable
     fun ColorButton(color: Color, isActive: Boolean, onClick: () -> Unit) {
         IconButton(
             onClick = onClick
         ) {
             if (isActive) {
-                Box(modifier = Modifier
-                    .size(15.dp)
-                    .clip(CircleShape)
-                    .background(color)
-                    .border(2.dp, Color.Gray, CircleShape))
+                Box(
+                    modifier = Modifier
+                        .size(15.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .border(2.dp, Color.Gray, CircleShape)
+                )
             } else {
-                Box(modifier = Modifier
-                    .size(15.dp)
-                    .clip(CircleShape)
-                    .background(color))
+                Box(
+                    modifier = Modifier
+                        .size(15.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                )
             }
         }
     }
-
 
 
     @Composable
     fun BrushDialogButton() {
         val showDialog = remember { mutableStateOf(false) }
         if (showDialog.value) {
-            BrushDialog(onDismiss = {showDialog.value = false})
+            BrushDialog(onDismiss = { showDialog.value = false })
         }
         IconButton(
             onClick = { showDialog.value = true },
@@ -225,9 +258,10 @@ enum class BrushSize {
     MEDIUM,
     BIG
 }
+
 @Composable
 fun BrushCircle(size: BrushSize) {
-    val sizeDp = when(size) {
+    val sizeDp = when (size) {
         BrushSize.SMALL -> 10.dp
         BrushSize.MEDIUM -> 20.dp
         BrushSize.BIG -> 30.dp
