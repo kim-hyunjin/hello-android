2625299a9a16e66c22b56687ca0ae0ceef3676c6
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-07 23:01:05 +0900

    [feat] 운동화면에 exercise timer 설정

--- udemy/WorkoutApp/app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
+++ udemy/WorkoutApp/app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
@@ -2,6 +2,7 @@ package com.example.workoutapp
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.workoutapp.databinding.ActivityExerciseBinding
@@ -11,6 +12,8 @@ class ExerciseActivity: AppCompatActivity() {
     private var binding: ActivityExerciseBinding? = null
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,11 +40,42 @@ class ExerciseActivity: AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "we will start the exercise.", Toast.LENGTH_LONG).show()
+                setExerciseView()
             }
 
         }.start()
 
+    }
+
+    private fun setExerciseView() {
+        binding?.flProgressBar?.visibility = View.INVISIBLE
+        binding?.flExerciseView?.visibility = View.VISIBLE
+        setExerciseProgressBar()
+    }
+
+    private fun setExerciseProgressBar() {
+        if (exerciseTimer != null) {
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+
+        binding?.progressBarExercise?.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = 30 - exerciseProgress
+                binding?.tvTimerExercise?.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "This is 30 seconds completed so now we will add all the exercises.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
     }
 
     override fun onDestroy() {
