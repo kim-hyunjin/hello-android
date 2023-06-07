afa2ced22d371e9d1758c6c27420b793c7c6a878
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-07 23:06:18 +0900

    [feat] 운동 모델, 데이터 추가

---
+++ udemy/WorkoutApp/app/src/main/java/com/example/workoutapp/models/ExerciseModel.kt
@@ -0,0 +1,9 @@
+package com.example.workoutapp.models
+
+data class ExerciseModel(
+    private var id: Int,
+    private var name: String,
+    private var image: Int,
+    private var isCompleted: Boolean,
+    private var isSelected: Boolean
+)
