955e51fa0317d5cff4ba51ec8ec0e4bb27170e87
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-07 15:02:12 +0900

    [feat] viewmodel pager 셋팅 - RxPagingSource 클래스 추가 - Flowable<PagingData<Movie>> 필드 추가 - caches a Flowable of PagingData within a CoroutineScope.

--- udemy/MovieApp/app/src/main/java/com/example/movieapp/viewmodel/MainActivityViewModel.java
+++ udemy/MovieApp/app/src/main/java/com/example/movieapp/viewmodel/MainActivityViewModel.java
@@ -3,23 +3,75 @@ package com.example.movieapp.viewmodel;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelKt;
+import androidx.paging.Pager;
+import androidx.paging.PagingConfig;
+import androidx.paging.PagingData;
+import androidx.paging.PagingState;
+import androidx.paging.rxjava3.PagingRx;
+import androidx.paging.rxjava3.RxPagingSource;
 
 import com.example.movieapp.model.Movie;
 import com.example.movieapp.model.MovieRepository;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Single;
+import kotlinx.coroutines.CoroutineScope;
+
 public class MainActivityViewModel extends AndroidViewModel {
-    private MovieRepository movieRepository;
+    private final MovieRepository movieRepository;
+    public Flowable<PagingData<Movie>> moviePagingDataFlowable;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
         movieRepository = new MovieRepository(application);
+        init();
     }
 
-    public LiveData<List<Movie>> getAllMovies() {
-        return movieRepository.getMutableLiveData();
+
+    private void init() {
+        MoviePagingSource moviePagingSource = new MoviePagingSource();
+
+        Pager<Integer, Movie> pager = new Pager<>(
+                new PagingConfig(20, 20, false, 20, 20*499),
+                () -> moviePagingSource
+        );
+
+        moviePagingDataFlowable = PagingRx.getFlowable(pager);
+        CoroutineScope coroutineScope = ViewModelKt.getViewModelScope(this);
+        PagingRx.cachedIn(moviePagingDataFlowable, coroutineScope);
+    }
+
+    class MoviePagingSource extends RxPagingSource<Integer, Movie> {
+
+
+        @Nullable
+        @Override
+        public Integer getRefreshKey(@NonNull PagingState<Integer, Movie> pagingState) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Single<LoadResult<Integer, Movie>> loadSingle(@NonNull LoadParams<Integer> loadParams) {
+            try {
+
+                int page = loadParams.getKey() != null ? loadParams.getKey() : 1;
+
+                return movieRepository.getMovies(page)
+                        .map(movies -> toLoadResult(movies, page))
+                        .onErrorReturn(LoadResult.Error::new);
+            } catch (Exception e) {
+                return Single.just(new LoadResult.Error<>(e));
+            }
+        }
+
+        private LoadResult<Integer, Movie> toLoadResult(List<Movie> movies, int page) {
+            return new LoadResult.Page<>(movies, page == 1 ? null : page - 1, page + 1);
+        }
     }
 }
