9c9c39c93a785b85d547067c528ed00c1536c758
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-11 21:27:06 +0900

    [feat] 운동 종료 후 기록 db에 저장

--- udemy/WorkoutApp/app/src/main/java/com/example/workoutapp/FinishActivity.kt
+++ udemy/WorkoutApp/app/src/main/java/com/example/workoutapp/FinishActivity.kt
@@ -2,7 +2,15 @@ package com.example.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.example.workoutapp.database.HistoryDao
+import com.example.workoutapp.database.HistoryEntity
 import com.example.workoutapp.databinding.ActivityFinishBinding
+import com.example.workoutapp.extensions.toStringFormat
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class FinishActivity : AppCompatActivity() {
     private var binding: ActivityFinishBinding? = null
@@ -16,6 +24,17 @@ class FinishActivity : AppCompatActivity() {
 
         binding?.btnFinish?.setOnClickListener {
             finish()
+        }
+
+        val dao = (application as WorkOutApp).db.historyDao()
+        addDateToDatabase(dao)
+    }
+
+    private fun addDateToDatabase(historyDao: HistoryDao) {
+        val dateTime = Calendar.getInstance().time
+
+        lifecycleScope.launch {
+            historyDao.insert(HistoryEntity(date = dateTime.toStringFormat()))
         }
     }
 }
