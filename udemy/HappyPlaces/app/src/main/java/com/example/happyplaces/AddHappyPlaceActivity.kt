cff703ae161cfafa208806bb689ece2b2876aef8
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-12 13:58:58 +0900

    [add] 앱 테마 설정, add happy place activity 추가

---
+++ udemy/HappyPlaces/app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,17 @@
+package com.example.happyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityAddHappyPlaceBinding
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddHappyPlaceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
