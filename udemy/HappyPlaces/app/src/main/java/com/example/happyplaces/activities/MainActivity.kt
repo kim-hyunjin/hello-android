46c5513793f49e0b966301797c5c937af0f8cf7d
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-12 22:27:58 +0900

    [feat] db의 place 목록 recyclerview에 렌더링

--- udemy/HappyPlaces/app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ udemy/HappyPlaces/app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -3,7 +3,15 @@ package com.example.happyplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.happyplaces.HappyPlaceApp
+import com.example.happyplaces.adapters.HappyPlaceAdapter
 import com.example.happyplaces.databinding.ActivityMainBinding
+import com.example.happyplaces.models.PlaceEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,5 +24,35 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
         }
+
+        initHappyPlacesView()
+    }
+
+    private fun initHappyPlacesView() {
+        val placeDao = (application as HappyPlaceApp).db.placeDao()
+
+        // 아래 코틀린 스코프를 사용하는 부분은 어떻게 짜야 베스트인지 모르겠다..
+        lifecycleScope.launch(Dispatchers.IO) {
+            placeDao.fetchAllPlace().collect {
+                val isDataExist = it.isNotEmpty()
+
+                runOnUiThread {
+                    if (isDataExist) {
+                        setupHappyPlaceRecyclerView(it)
+                        binding.rvHappyPlaceList.visibility = View.VISIBLE
+                        binding.tvNoPlaceData.visibility = View.GONE
+                    } else {
+                        binding.rvHappyPlaceList.visibility = View.GONE
+                        binding.tvNoPlaceData.visibility = View.VISIBLE
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun setupHappyPlaceRecyclerView(places: List<PlaceEntity>) {
+        binding.rvHappyPlaceList.layoutManager = LinearLayoutManager(this)
+        binding.rvHappyPlaceList.adapter = HappyPlaceAdapter(places)
     }
 }
---
+++ udemy/HappyPlaces/app/src/main/java/com/example/happyplaces/adapters/HappyPlaceAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.happyplaces.adapters
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.happyplaces.databinding.ItemHappyPlaceBinding
+import com.example.happyplaces.models.PlaceEntity
+
+class HappyPlaceAdapter(private val places: List<PlaceEntity>):RecyclerView.Adapter<HappyPlaceAdapter.ViewHolder>() {
+
+    class ViewHolder(binding: ItemHappyPlaceBinding): RecyclerView.ViewHolder(binding.root) {
+        val image = binding.ivPlaceImage
+        val title = binding.tvTitle
+        val description = binding.tvDescription
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemHappyPlaceBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val place = places[position]
+        holder.image.setImageURI(Uri.parse(place.image))
+        holder.title.text = place.title
+        holder.description.text = place.description
+    }
+}
