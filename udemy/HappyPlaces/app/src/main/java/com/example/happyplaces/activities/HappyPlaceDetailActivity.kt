2422c0cb4cc8200cb25a2a11efec2ecac7fafbf9
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-14 17:19:43 +0900

    [feat] 상세 페이지에서 location 맵으로 보기 수정

--- udemy/HappyPlaces/app/src/main/java/com/example/happyplaces/activities/HappyPlaceDetailActivity.kt
+++ udemy/HappyPlaces/app/src/main/java/com/example/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -1,14 +1,15 @@
 package com.example.happyplaces.activities
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.happyplaces.R
 import com.example.happyplaces.databinding.ActivityHappyPlaceDetailBinding
 import com.example.happyplaces.models.PlaceEntity
 
 class HappyPlaceDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHappyPlaceDetailBinding
+    private var place: PlaceEntity? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHappyPlaceDetailBinding.inflate(layoutInflater)
@@ -16,17 +17,18 @@ class HappyPlaceDetailActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbarHappyPlaceDetail)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        var place: PlaceEntity? = null
         if (intent.hasExtra("place")) {
             place = intent.getParcelableExtra("place")
         }
         if (place != null) {
-            supportActionBar?.title = place.title
-            binding.ivPlaceImage.setImageURI(Uri.parse(place.image))
-            binding.tvDescription.text = place.description
-            binding.tvLocation.text = place.location
+            supportActionBar?.title = place!!.title
+            binding.ivPlaceImage.setImageURI(Uri.parse(place!!.image))
+            binding.tvDescription.text = place!!.description
+            binding.tvLocation.text = place!!.location
             binding.btnViewOnMap.setOnClickListener {
-                supportFragmentManager.beginTransaction().replace(R.id.fragment_container_view, MapsFragment(place)).commit()
+                val intent = Intent(this, MapActivity::class.java)
+                intent.putExtra("place", place)
+                startActivity(intent)
             }
         }
 
