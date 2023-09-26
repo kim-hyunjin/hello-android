7e6c3c207bda7658f95d511fcbac3eb0191c80be
kim-hyunjin <khj@devtools.kr>
2023-09-27 00:11:11 +0900

    액티비티 전환

--- fastcampus/app/src/main/java/com/github/kimhyunjin/emergencymedicalinfoapp/MainActivity.kt
+++ fastcampus/app/src/main/java/com/github/kimhyunjin/emergencymedicalinfoapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.github.kimhyunjin.emergencymedicalinfoapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.kimhyunjin.emergencymedicalinfoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goInputActivityButton.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            intent.putExtra("msg", "응급의료정보")
+            startActivity(intent)
+        }
     }
 }
