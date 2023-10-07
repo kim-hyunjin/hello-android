463c624f0685908c08d6021383f41ef9acb4b1cd
kim-hyunjin <khj@devtools.kr>
2023-10-08 00:17:04 +0900

    save data to SharedPreference

--- fastcampus/app/src/main/java/com/github/kimhyunjin/emergencymedicalinfoapp/EditActivity.kt
+++ fastcampus/app/src/main/java/com/github/kimhyunjin/emergencymedicalinfoapp/EditActivity.kt
@@ -2,9 +2,11 @@ package com.github.kimhyunjin.emergencymedicalinfoapp
 
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.core.view.isVisible
 import com.github.kimhyunjin.emergencymedicalinfoapp.databinding.ActivityEditBinding
 
@@ -15,7 +17,11 @@ class EditActivity : AppCompatActivity() {
         binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bloodTypeSpinner.adapter = ArrayAdapter.createFromResource(this, R.array.blood_types, android.R.layout.simple_list_item_1)
+        binding.bloodTypeSpinner.adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.blood_types,
+            android.R.layout.simple_list_item_1
+        )
 
         binding.birthLayer.setOnClickListener {
             val listener = OnDateSetListener { date, year, month, dayOfMonth ->
@@ -29,5 +35,33 @@ class EditActivity : AppCompatActivity() {
         }
 
         binding.warningValueEditText.isVisible = binding.warningCheckBox.isChecked
+
+        binding.saveButton.setOnClickListener {
+            saveData()
+            finish()
+        }
+    }
+
+    private fun saveData() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE).edit()) {
+
+            putString(NAME, binding.nameValueEditText.text.toString())
+            putString(BLOOD_TYPE, getBloodType())
+            putString(EMERGENCY_CONTACT, binding.emergencyContactValueEditText.text.toString())
+            putString(BIRTH_DATE, binding.birthValueTextView.text.toString())
+            putString(WARNING, getWarning())
+            apply()
+        }
+        Toast.makeText(this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getBloodType(): String {
+        val bloodAlphabet = binding.bloodTypeSpinner.selectedItem.toString()
+        val bloodSign = if (binding.bloodTypePlus.isChecked) "+" else "-"
+        return "$bloodSign$bloodAlphabet"
+    }
+
+    private fun getWarning(): String {
+        return if (binding.warningCheckBox.isChecked) binding.warningValueEditText.text.toString() else ""
     }
 }
--- fastcampus/app/src/main/java/com/github/kimhyunjin/emergencymedicalinfoapp/MainActivity.kt
+++ fastcampus/app/src/main/java/com/github/kimhyunjin/emergencymedicalinfoapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.github.kimhyunjin.emergencymedicalinfoapp
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.github.kimhyunjin.emergencymedicalinfoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,26 @@ class MainActivity : AppCompatActivity() {
         binding.goEditActivityButton.setOnClickListener {
             val intent = Intent(this, EditActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getDataAndUIUpdate()
+    }
+
+    private fun getDataAndUIUpdate() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE)) {
+            binding.nameValueTextView.text = getString(NAME, "미정")
+            binding.birthValueTextView.text = getString(BIRTH_DATE, "미정")
+            binding.bloodTypeValueTextView.text = getString(BLOOD_TYPE, "미정")
+            binding.emergencyContactValueTextView.text = getString(EMERGENCY_CONTACT, "미정")
+            val warning = getString(NAME, "")
+            binding.warningTextView.isVisible = warning.isNullOrEmpty().not()
+            binding.warningValueTextView.isVisible = warning.isNullOrEmpty().not()
+            if (!warning.isNullOrBlank()) {
+                binding.warningValueTextView.text = getString(NAME, "미정")
+            }
         }
     }
 }
