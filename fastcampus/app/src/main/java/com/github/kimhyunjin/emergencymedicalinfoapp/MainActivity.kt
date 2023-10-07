190d8a8dc4af3796877c3da0ef4aa1d7685039c5
kim-hyunjin <khj@devtools.kr>
2023-10-07 18:29:22 +0900

    edit activity layout 작업

--- fastcampus/app/src/main/java/com/github/kimhyunjin/emergencymedicalinfoapp/MainActivity.kt
+++ fastcampus/app/src/main/java/com/github/kimhyunjin/emergencymedicalinfoapp/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.goInputActivityButton.setOnClickListener {
+        binding.goEditActivityButton.setOnClickListener {
             val intent = Intent(this, EditActivity::class.java)
             intent.putExtra("msg", "응급의료정보")
             startActivity(intent)
