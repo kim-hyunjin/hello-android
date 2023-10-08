46e151866ea6785dc2dd7d4dbe8bd3bcecd96093
kim-hyunjin <khj@devtools.kr>
2023-10-08 16:01:40 +0900

    use bigdecimal and decimal formatting

--- Calculator/app/src/main/java/com/github/kimhyunjin/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/github/kimhyunjin/calculator/MainActivity.kt
@@ -2,17 +2,19 @@ package com.github.kimhyunjin.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.github.kimhyunjin.calculator.databinding.ActivityMainBinding
+import java.math.BigDecimal
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val number1 = StringBuilder()
     private val number2 = StringBuilder()
     private val op = StringBuilder()
+    private val decimalFormat = DecimalFormat("#,###")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -39,14 +41,13 @@ class MainActivity : AppCompatActivity() {
         if (op.isEmpty()) {
             Toast.makeText(this, "올바르지 않은 수식입니다.", Toast.LENGTH_SHORT).show()
         }
-        val num1 = if (number1.isEmpty()) 0 else number1.toString().toInt()
-        val num2 = if (number2.isEmpty()) 0 else number2.toString().toInt()
+        val num1 = if (number1.isEmpty()) BigDecimal(0) else number1.toString().toBigDecimal()
+        val num2 = if (number2.isEmpty()) BigDecimal(0) else number2.toString().toBigDecimal()
 
-        Log.d("equalClicked", op.toString())
         val result = if (op.toString().equals("+")) {
-            num1 + num2
+            decimalFormat.format(num1 + num2)
         } else {
-            num1 - num2
+            decimalFormat.format(num1 - num2)
         }
 
         number1.clear()
@@ -59,10 +60,9 @@ class MainActivity : AppCompatActivity() {
         val opString = (view as? Button)?.text.toString() ?: ""
 
         if (number1.isEmpty()) {
-            val resultViewText = binding.resultTextView.text
+            val resultViewText = binding.resultTextView.text.toString()
             if (resultViewText.isNotEmpty()) {
-                number1.clear()
-                number1.append(resultViewText)
+                number1.append(resultViewText.replace(",", ""))
             } else {
                 number1.append("0")
             }
@@ -80,6 +80,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateEquationTextView() {
-        binding.equationTextView.text = "$number1 $op $number2"
+        val formattedNum1 = if (number1.isNotEmpty()) decimalFormat.format(
+            number1.toString().toBigDecimal()
+        ) else ""
+        val formattedNum2 = if (number2.isNotEmpty()) decimalFormat.format(
+            number2.toString().toBigDecimal()
+        ) else ""
+        binding.equationTextView.text = "$formattedNum1 $op $formattedNum2"
     }
 }
