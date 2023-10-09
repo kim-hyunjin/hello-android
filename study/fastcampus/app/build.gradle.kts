52e54baf6b52b77ebf4886c79823a68aea3e0a49
kim-hyunjin <khj@devtools.kr>
2023-10-09 21:24:59 +0900

    단어장 앱 - fix Room setting - use ksp

--- study/fastcampus/app/build.gradle.kts
+++ study/fastcampus/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -51,5 +52,5 @@ dependencies {
     val room_version = "2.5.0"
 
     implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
 }
