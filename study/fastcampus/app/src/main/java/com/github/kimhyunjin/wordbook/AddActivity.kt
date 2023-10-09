21cc00422e6d3071203eefe0f94ba70a951d55d5
kim-hyunjin <khj@devtools.kr>
2023-10-09 21:28:30 +0900

    단어장 앱 - room에 단어 저장

--- study/fastcampus/app/src/main/java/com/github/kimhyunjin/wordbook/AddActivity.kt
+++ study/fastcampus/app/src/main/java/com/github/kimhyunjin/wordbook/AddActivity.kt
@@ -1,11 +1,12 @@
 package com.github.kimhyunjin.wordbook
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kimhyunjin.wordbook.databinding.ActivityAddBinding
 import com.google.android.material.chip.Chip
 
-class AddActivity: AppCompatActivity() {
+class AddActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,10 @@ class AddActivity: AppCompatActivity() {
         setContentView(binding.root)
 
         initViews()
+
+        binding.addButton.setOnClickListener {
+            add()
+        }
     }
 
     private fun initViews() {
@@ -31,5 +36,23 @@ class AddActivity: AppCompatActivity() {
             isCheckable = true
             isClickable = true
         }
+    }
+
+    private fun add() {
+        val text = binding.textInputEditText.text.toString()
+        val mean = binding.meanTextInputEditText.text.toString()
+        val type = findViewById<Chip>(binding.typeChipGroup.checkedChipId).text.toString()
+
+        val word = Word(text, mean, type)
+
+        Thread {
+            AppDatabase.getInstance(this).wordDao().insert(word)
+            runOnUiThread {
+
+                Toast.makeText(this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show()
+            }
+            finish()
+        }.start()
+
     }
 }
