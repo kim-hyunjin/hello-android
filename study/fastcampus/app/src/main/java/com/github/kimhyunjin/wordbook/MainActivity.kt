ce0c8143b6cf5a055e23a793d429785afbfe48aa
kim-hyunjin <khj@devtools.kr>
2023-10-09 20:35:49 +0900

    단어장 앱 - word list recycler view

--- study/fastcampus/app/src/main/java/com/github/kimhyunjin/wordbook/MainActivity.kt
+++ study/fastcampus/app/src/main/java/com/github/kimhyunjin/wordbook/MainActivity.kt
@@ -2,10 +2,46 @@ package com.github.kimhyunjin.wordbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import com.github.kimhyunjin.wordbook.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), WordAdapter.ItemWordClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var wordAdapter: WordAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        val words = mutableListOf<Word>(
+            Word("weather", "날씨", "명사"),
+            Word("weather", "날씨", "명사"),
+            Word("weather", "날씨", "명사")
+        )
+
+        wordAdapter = WordAdapter(words, this)
+        binding.wordRecyclerView.apply {
+            adapter = wordAdapter
+            layoutManager =
+                LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            addItemDecoration(
+                DividerItemDecoration(
+                    applicationContext,
+                    LinearLayoutManager.VERTICAL
+                )
+            )
+        }
+    }
+
+    override fun onClick(word: Word) {
+        Toast.makeText(this, "${word.text} clicked1", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ study/fastcampus/app/src/main/java/com/github/kimhyunjin/wordbook/Word.kt
@@ -0,0 +1,3 @@
+package com.github.kimhyunjin.wordbook
+
+data class Word(val text: String, val mean: String, val type: String)
