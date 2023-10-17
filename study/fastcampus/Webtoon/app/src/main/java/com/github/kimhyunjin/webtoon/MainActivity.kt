a898faa913ece0cc1638e0011dcb36ffc81d6227
kim-hyunjin <khj@devtools.kr>
2023-10-17 22:48:25 +0900

    webview 사용하기

---
+++ study/fastcampus/Webtoon/app/src/main/java/com/github/kimhyunjin/webtoon/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.github.kimhyunjin.webtoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://google.com")
+    }
+}
