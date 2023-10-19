0b8a3e4a0d7aaa1ed27963e6cac65d819d2fc8bd
kim-hyunjin <khj@devtools.kr>
2023-10-19 21:06:25 +0900

    녹음, 재생기능 구현

--- study/fastcampus/AudioRecorder/app/src/main/java/com/github/kimhyunjin/audiorecorder/MainActivity.kt
+++ study/fastcampus/AudioRecorder/app/src/main/java/com/github/kimhyunjin/audiorecorder/MainActivity.kt
@@ -3,45 +3,99 @@ package com.github.kimhyunjin.audiorecorder
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.github.kimhyunjin.audiorecorder.databinding.ActivityMainBinding
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
+    private var fileName: String = ""
+
+    private enum class State {
+        RELEASE, RECORDING, PLAYING, PAUSE
+    }
+    private var state: State = State.RELEASE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
+        fileName = "${externalCacheDir?.absoluteFile}/record_test.3gp"
+
         binding.recordButton.setOnClickListener {
-            when {
-                ContextCompat.checkSelfPermission(
-                    this@MainActivity,
-                    Manifest.permission.RECORD_AUDIO
-                ) == PackageManager.PERMISSION_GRANTED -> {
-                    // TODO 녹음 시작
+            when(state) {
+                State.RELEASE -> {
+                    record()
                 }
-
-                ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO) -> {
-                    showPermissionRationalDialog()
+                State.RECORDING -> {
+                    stopRecording()
                 }
-
                 else -> {
-                    ActivityCompat.requestPermissions(
-                        this,
-                        arrayOf(Manifest.permission.RECORD_AUDIO),
-                        REQUEST_RECORD_AUDIO
-                    )
+
                 }
+            }
+        }
+
+        binding.playButton.setOnClickListener {
+            when(state) {
+                State.RELEASE -> {
+                    startPlaying()
+                }
+                State.RECORDING -> {
+
+                }
+                State.PLAYING -> {
+                    pausePlaying()
+                }
+                State.PAUSE -> {
+                    resumePlaying()
+                }
+            }
+        }
+
+        binding.stopButton.setOnClickListener {
+            stopPlaying()
+        }
+    }
+
+    private fun record() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                startRecording()
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) -> {
+                showPermissionRationalDialog()
+            }
+
+            else -> {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.RECORD_AUDIO),
+                    REQUEST_RECORD_AUDIO
+                )
             }
         }
     }
@@ -81,6 +135,104 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    private fun startRecording() {
+        state = State.RECORDING
+        recorder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MediaRecorder(this)
+        } else {
+            MediaRecorder()
+        }
+        recorder?.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e("APP", "MediaRecorder prepare() failed $e")
+            }
+
+            start()
+        }
+
+        binding.recordButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.baseline_stop_24))
+        binding.recordButton.imageTintList = ColorStateList.valueOf(Color.BLACK)
+        binding.playButton.isEnabled = false
+        binding.playButton.alpha = 0.3f
+        binding.stopButton.isEnabled = false
+        binding.stopButton.alpha = 0.3f
+    }
+
+    private fun stopRecording() {
+        state = State.RELEASE
+        recorder?.apply {
+            stop()
+            release()
+        }
+
+        binding.recordButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.baseline_fiber_manual_record_24))
+        binding.recordButton.imageTintList = ColorStateList.valueOf(Color.RED)
+        binding.playButton.isEnabled = true
+        binding.playButton.alpha = 1.0f
+        binding.stopButton.isEnabled = true
+        binding.stopButton.alpha = 1.0f
+    }
+
+    private fun startPlaying() {
+        state = State.PLAYING
+
+        player = MediaPlayer().apply {
+            setDataSource(fileName)
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e("APP", "MediaPlayer prepare() failed $e")
+            }
+
+            start()
+
+            setOnCompletionListener {
+                state = State.RELEASE
+                binding.recordButton.isEnabled = true
+                binding.recordButton.alpha = 1.0f
+            }
+        }
+
+        binding.recordButton.isEnabled = false
+        binding.recordButton.alpha = 0.3f
+        binding.playButton.setImageResource(R.drawable.baseline_pause_24)
+    }
+
+    private fun pausePlaying() {
+        player?.apply {
+            state = State.PAUSE
+            pause()
+        }
+
+        binding.playButton.setImageResource(R.drawable.baseline_play_arrow_24)
+    }
+
+    private fun resumePlaying() {
+        player?.apply {
+            state = State.PLAYING
+            start()
+        }
+
+        binding.playButton.setImageResource(R.drawable.baseline_pause_24)
+    }
+
+    private fun stopPlaying() {
+        state = State.RELEASE
+
+        player?.release()
+        player = null
+
+        binding.recordButton.isEnabled = true
+        binding.recordButton.alpha = 1.0f
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -92,7 +244,7 @@ class MainActivity : AppCompatActivity() {
             requestCode == REQUEST_RECORD_AUDIO && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
 
         if (audioRecordPermissionGranted) {
-            // TODO 녹음 시작
+            startRecording()
         } else {
             showPermissionSettingDialog()
         }
