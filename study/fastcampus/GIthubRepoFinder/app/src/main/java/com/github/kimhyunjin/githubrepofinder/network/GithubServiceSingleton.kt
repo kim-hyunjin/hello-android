2a519b3b07be5325a763233fa256a612569d0665
kim-hyunjin <khj@devtools.kr>
2023-10-22 13:54:31 +0900

    refactoring, ripple 효과 추가

--- study/fastcampus/GIthubRepoFinder/app/src/main/java/com/github/kimhyunjin/githubrepofinder/network/GithubServiceSingleton.kt
+++ study/fastcampus/GIthubRepoFinder/app/src/main/java/com/github/kimhyunjin/githubrepofinder/network/GithubServiceSingleton.kt
@@ -8,6 +8,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class GithubServiceSingleton private constructor() {
     companion object {
+        private const val BASE_URL = "https://api.github.com/"
+
         private var githubService: GithubService? = null
 
         fun getInstance(context: Context): GithubService {
@@ -17,11 +19,14 @@ class GithubServiceSingleton private constructor() {
                 val client = OkHttpClient.Builder().addInterceptor { chain ->
                     val newReq =
                         chain.request().newBuilder()
-                            .addHeader("Authorization", "Bearer ${context.getString(R.string.GITHUB_API_KEY)}").build()
+                            .addHeader(
+                                "Authorization",
+                                "Bearer ${context.getString(R.string.GITHUB_API_KEY)}"
+                            ).build()
                     chain.proceed(newReq)
                 }.build()
 
-                val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/").client(client)
+                val retrofit = Retrofit.Builder().baseUrl(BASE_URL).client(client)
                     .addConverterFactory(GsonConverterFactory.create()).build()
 
                 githubService = retrofit.create(GithubService::class.java)
