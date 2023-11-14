b0f17fc27cfc9693fa1edf4f2cf9923d14809239
kim-hyunjin <hyunjin1612@gmail.com>
2023-11-14 22:42:12 +0900

    가져온 기상예보 데이터로 UI 그리기

--- study/fastcampus/WeatherApp/app/src/main/java/com/github/kimhyunjin/weatherapp/Forecast.kt
+++ study/fastcampus/WeatherApp/app/src/main/java/com/github/kimhyunjin/weatherapp/Forecast.kt
@@ -7,7 +7,16 @@ data class Forecast(
     var sky: SkyType? = null,
     var precipitation: Int = 0,
     var precipitationType: PrecipitationType = PrecipitationType.NONE,
-)
+) {
+    val weather: String
+        get() {
+            return if (precipitationType == PrecipitationType.NONE) {
+                sky?.getString() ?: ""
+            } else {
+                precipitationType.getString()
+            }
+        }
+}
 
 enum class PrecipitationType(val code: Int) {
     NONE(0),
@@ -21,6 +30,17 @@ enum class PrecipitationType(val code: Int) {
             return PrecipitationType.values().first { it.code == code }
         }
     }
+
+    fun getString(): String {
+        return when (this.code) {
+            0 -> ""
+            1 -> "비"
+            2 -> "비 또는 눈"
+            3 -> "눈"
+            4 -> "소나기"
+            else -> ""
+        }
+    }
 }
 
 enum class SkyType(val code: Int) {
@@ -31,6 +51,15 @@ enum class SkyType(val code: Int) {
     companion object {
         fun fromCode(code: Int): SkyType {
             return SkyType.values().first { it.code == code }
+        }
+    }
+
+    fun getString(): String {
+        return when (this.code) {
+            1 -> "맑음"
+            3 -> "구름많음"
+            4 -> "비"
+            else -> ""
         }
     }
 }
--- study/fastcampus/WeatherApp/app/src/main/java/com/github/kimhyunjin/weatherapp/MainActivity.kt
+++ study/fastcampus/WeatherApp/app/src/main/java/com/github/kimhyunjin/weatherapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.github.kimhyunjin.weatherapp
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,8 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import com.github.kimhyunjin.weatherapp.databinding.ActivityMainBinding
+import com.github.kimhyunjin.weatherapp.databinding.ItemForecastBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.gson.GsonBuilder
@@ -20,16 +23,18 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private val locationRequestPermission =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
             when {
                 permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                    updateLocation()
+                    updateForecastWithLastLocation()
                 }
 
                 else -> {
@@ -45,14 +50,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         locationRequestPermission.launch(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION))
     }
 
-    private fun updateLocation() {
+    private fun updateForecastWithLastLocation() {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_COARSE_LOCATION
@@ -65,9 +71,26 @@ class MainActivity : AppCompatActivity() {
         fusedLocationClient.lastLocation.addOnSuccessListener {
             if (it != null) {
                 Log.i("Location", "${it.latitude}, ${it.longitude}")
+                updateLocationTextView(it)
                 fetchForecast(it)
             }
         }
+
+    }
+
+    private fun updateLocationTextView(location: Location) {
+        Thread {
+            val addressList = Geocoder(this, Locale.KOREA).getFromLocation(
+                location.latitude,
+                location.longitude,
+                1
+            )
+            addressList?.first().let {
+                runOnUiThread {
+                    binding.locationTextView.text = it?.thoroughfare
+                }
+            }
+        }.start()
     }
 
     private fun fetchForecast(location: Location) {
@@ -94,42 +117,8 @@ class MainActivity : AppCompatActivity() {
                 call: Call<WeatherResponse>,
                 response: Response<WeatherResponse>
             ) {
-                val weatherDateTimeMap = mutableMapOf<String, Forecast>()
-                val weatherList = response.body()?.response?.body?.items?.entities
-
-                weatherList?.forEach {
-                    Log.i("Weather", it.toString())
-                    val key = "${it.forecastDate}/${it.forecastTime}"
-                    weatherDateTimeMap.putIfAbsent(
-                        key,
-                        Forecast(date = it.forecastDate, time = it.forecastTime)
-                    )
-
-                    weatherDateTimeMap[key].apply {
-                        if (this != null && it.category != null) {
-                            when (it.category) {
-                                Category.POP -> {
-                                    precipitation = it.forecastValue.toInt()
-                                }
-
-                                Category.PTY -> {
-                                    precipitationType =
-                                        PrecipitationType.fromCode(it.forecastValue.toInt())
-                                }
-
-                                Category.SKY -> {
-                                    sky = SkyType.fromCode(it.forecastValue.toInt())
-                                }
-
-                                Category.TMP -> {
-                                    temperature = it.forecastValue.toDouble()
-                                }
-                            }
-                        }
-                    }
-                }
-
-                Log.i("Forecast", weatherDateTimeMap.toString())
+                val forecasts = deserializeForecastResponse(response)
+                updateForecastUI(forecasts)
             }
 
             override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
@@ -137,5 +126,78 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun deserializeForecastResponse(response: Response<WeatherResponse>): List<Forecast> {
+        val weatherList = response.body()?.response?.body?.items?.entities
+
+        val weatherDateTimeMap = mutableMapOf<String, Forecast>()
+        weatherList?.forEach {
+            Log.i("Weather", it.toString())
+            val key = "${it.forecastDate}/${it.forecastTime}"
+            weatherDateTimeMap.putIfAbsent(
+                key,
+                Forecast(date = it.forecastDate, time = it.forecastTime)
+            )
+
+            weatherDateTimeMap[key].apply {
+                if (this != null && it.category != null) {
+                    when (it.category) {
+                        Category.POP -> {
+                            precipitation = it.forecastValue.toInt()
+                        }
+
+                        Category.PTY -> {
+                            precipitationType =
+                                PrecipitationType.fromCode(it.forecastValue.toInt())
+                        }
+
+                        Category.SKY -> {
+                            sky = SkyType.fromCode(it.forecastValue.toInt())
+                        }
+
+                        Category.TMP -> {
+                            temperature = it.forecastValue.toDouble()
+                        }
+                    }
+                }
+            }
+        }
+
+        Log.i("Forecast", weatherDateTimeMap.toString())
+        val list = weatherDateTimeMap.values.toMutableList()
+        list.sortWith { f1, f2 ->
+            val f1DateTime = "${f1.date}${f1.time}"
+            val f2DateTime = "${f2.date}${f1.time}"
+
+            return@sortWith f1DateTime.compareTo(f2DateTime)
+        }
+
+        return list
+    }
+
+    private fun updateForecastUI(forecasts: List<Forecast>) {
+        val currentForecast = forecasts.first()
+        binding.temperatureTextView.text =
+            getString(R.string.temperature_text, currentForecast.temperature)
+        binding.skyTextView.text = currentForecast.weather
+        binding.precipitationTextView.text =
+            getString(R.string.precipication_text, currentForecast.precipitation)
+        binding.childForecastLayout.apply {
+
+            forecasts.forEachIndexed { index, forecast ->
+                if (index == 0) {
+                    return@forEachIndexed
+                }
+                val itemView = ItemForecastBinding.inflate(layoutInflater)
+                itemView.timeTextView.text = forecast.time
+                itemView.weatherTextView.text = forecast.weather
+                itemView.temperatureTextView.text =
+                    getString(R.string.temperature_text, forecast.temperature)
+
+                addView(itemView.root)
+            }
+
+        }
     }
 }
