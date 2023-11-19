4f69b7c52a7b2ce26ac7ba887aed2c28ad600c1d
kim-hyunjin <hyunjin1612@gmail.com>
2023-11-19 14:53:36 +0900

    refactoring: WeatherRepository

--- study/fastcampus/WeatherApp/app/src/main/java/com/github/kimhyunjin/weatherapp/MainActivity.kt
+++ study/fastcampus/WeatherApp/app/src/main/java/com/github/kimhyunjin/weatherapp/MainActivity.kt
@@ -8,21 +8,15 @@ import android.location.Location
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.github.kimhyunjin.weatherapp.WeatherRepository.fetchForecast
 import com.github.kimhyunjin.weatherapp.databinding.ActivityMainBinding
 import com.github.kimhyunjin.weatherapp.databinding.ItemForecastBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.gson.GsonBuilder
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -79,7 +73,9 @@ class MainActivity : AppCompatActivity() {
         targetLocation.latitude = 37.5635694444444
         targetLocation.longitude = 126.980008333333
         updateLocationTextView(targetLocation)
-        fetchForecast(targetLocation)
+        fetchForecast(targetLocation, onSuccess = {
+            updateForecastUI(it)
+        })
     }
 
     private fun updateLocationTextView(location: Location) {
@@ -95,89 +91,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }.start()
-    }
-
-    private fun fetchForecast(location: Location) {
-        val gson = GsonBuilder().setLenient().create()
-
-        val retrofit =
-            Retrofit.Builder().baseUrl("https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/")
-                .addConverterFactory(
-                    GsonConverterFactory.create(gson)
-                ).build()
-
-        val service = retrofit.create(WeatherService::class.java)
-
-        val baseDateTime = BaseDateTime.getCurrentBaseDateTime()
-        val converter = GeoPointConverter()
-        val point = converter.convert(lat = location.latitude, lon = location.longitude)
-        service.getForecast(
-            baseDate = baseDateTime.baseDate,
-            baseTime = baseDateTime.baseTime,
-            nx = point.nx,
-            ny = point.ny
-        ).enqueue(object : Callback<WeatherResponse> {
-            override fun onResponse(
-                call: Call<WeatherResponse>,
-                response: Response<WeatherResponse>
-            ) {
-                val forecasts = deserializeForecastResponse(response)
-                updateForecastUI(forecasts)
-            }
-
-            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
-                t.printStackTrace()
-            }
-
-        })
-    }
-
-    private fun deserializeForecastResponse(response: Response<WeatherResponse>): List<Forecast> {
-        val weatherList = response.body()?.response?.body?.items?.entities
-
-        val weatherDateTimeMap = mutableMapOf<String, Forecast>()
-        weatherList?.forEach {
-            Log.i("Weather", it.toString())
-            val key = "${it.forecastDate}/${it.forecastTime}"
-            weatherDateTimeMap.putIfAbsent(
-                key,
-                Forecast(date = it.forecastDate, time = it.forecastTime)
-            )
-
-            weatherDateTimeMap[key].apply {
-                if (this != null && it.category != null) {
-                    when (it.category) {
-                        Category.POP -> {
-                            precipitation = it.forecastValue.toInt()
-                        }
-
-                        Category.PTY -> {
-                            precipitationType =
-                                PrecipitationType.fromCode(it.forecastValue.toInt())
-                        }
-
-                        Category.SKY -> {
-                            sky = SkyType.fromCode(it.forecastValue.toInt())
-                        }
-
-                        Category.TMP -> {
-                            temperature = it.forecastValue.toDouble()
-                        }
-                    }
-                }
-            }
-        }
-
-        Log.i("Forecast", weatherDateTimeMap.toString())
-        val list = weatherDateTimeMap.values.toMutableList()
-        list.sortWith { f1, f2 ->
-            val f1DateTime = "${f1.date}${f1.time}"
-            val f2DateTime = "${f2.date}${f1.time}"
-
-            return@sortWith f1DateTime.compareTo(f2DateTime)
-        }
-
-        return list
     }
 
     private fun updateForecastUI(forecasts: List<Forecast>) {
