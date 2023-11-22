22b9f5cd12c7cc709b7fd975de1f36b446c0880d
kim-hyunjin <hyunjin1612@gmail.com>
2023-11-22 23:16:04 +0900

    맛집 검색 및 마커 표시하기

--- study/fastcampus/FoodMap/app/src/main/java/com/github/kimhyunjin/foodmap/MainActivity.kt
+++ study/fastcampus/FoodMap/app/src/main/java/com/github/kimhyunjin/foodmap/MainActivity.kt
@@ -3,12 +3,15 @@ package com.github.kimhyunjin.foodmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import com.github.kimhyunjin.foodmap.databinding.ActivityMainBinding
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,13 +29,61 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         binding.mapView.onCreate(savedInstanceState)
         binding.mapView.getMapAsync(this)
 
-        SearchRepository.getGoodRestaurant("서울").enqueue(object: Callback<SearchResult> {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query.isNullOrEmpty()) return false
+
+                searchWithQuery(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                return false
+            }
+
+        })
+
+
+    }
+
+    private fun searchWithQuery(query: String) {
+        SearchRepository.getGoodRestaurant(query).enqueue(object : Callback<SearchResult> {
             override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
                 Log.i("search", response.body().toString())
+
+                val searchItemList = response.body()?.items.orEmpty()
+                if (searchItemList.isEmpty()) {
+                    Toast.makeText(this@MainActivity, "검색 결과가 없습니다.", Toast.LENGTH_SHORT).show()
+                    return
+                } else if (naverMap == null) {
+                    Toast.makeText(this@MainActivity, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                    return
+                }
+
+                val markerList = searchItemList.map {
+                    Marker(
+                        LatLng(
+                            it.mapy.toDouble() / 10000000,
+                            it.mapx.toDouble() / 10000000
+                        )
+                    ).apply {
+                        captionText = it.title
+                        map = naverMap
+                    }
+                }
+
+                Log.i("search", markerList.map { it.position }.toString())
+
+
+                val cameraUpdate = CameraUpdate.scrollTo(markerList.first().position)
+                    .animate(CameraAnimation.Easing)
+                naverMap!!.moveCamera(cameraUpdate)
             }
 
             override fun onFailure(call: Call<SearchResult>, t: Throwable) {
-                TODO("Not yet implemented")
+                Toast.makeText(this@MainActivity, "오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                return
             }
 
         })
