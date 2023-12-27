5cf98a75b0d3a5ae8d6c85ff7d6495e2ae254636
kim-hyunjin <hyunjin1612@gmail.com>
2023-12-28 01:18:20 +0900

    카카오 로그인 완료, 프로젝트 세팅 변경

--- study/fastcampus/ShareLocation/app/src/main/java/com/github/kimhyunjin/sharelocation/MainActivity.kt
+++ study/fastcampus/ShareLocation/app/src/main/java/com/github/kimhyunjin/sharelocation/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.github.kimhyunjin.sharelocation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.github.kimhyunjin.sharelocation.databinding.ActivityMainBinding
+import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val keyHash = Utility.getKeyHash(this)
-        Log.e("keyhash", keyHash)
+        val keyhash = Utility.getKeyHash(this)
+        Log.i("keyhash", keyhash)
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "1fee26e3619c1a35f974d497df14516f")
+
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
