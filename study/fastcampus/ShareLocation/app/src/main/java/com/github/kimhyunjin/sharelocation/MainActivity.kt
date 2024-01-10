de004f045f2b0761f7710177dc34f63e7598f17c
kim-hyunjin <hyunjin1612@gmail.com>
2024-01-10 21:56:50 +0900

    google map 사용하기

--- study/fastcampus/ShareLocation/app/src/main/java/com/github/kimhyunjin/sharelocation/MainActivity.kt
+++ study/fastcampus/ShareLocation/app/src/main/java/com/github/kimhyunjin/sharelocation/MainActivity.kt
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.github.kimhyunjin.sharelocation.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var googleMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,6 +29,22 @@ class MainActivity : AppCompatActivity() {
         // Kakao SDK 초기화
         KakaoSdk.init(this, "1fee26e3619c1a35f974d497df14516f")
 
-        startActivity(Intent(this, LoginActivity::class.java))
+//        startActivity(Intent(this, LoginActivity::class.java))
+
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        this.googleMap.addMarker(
+            MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney"))
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
