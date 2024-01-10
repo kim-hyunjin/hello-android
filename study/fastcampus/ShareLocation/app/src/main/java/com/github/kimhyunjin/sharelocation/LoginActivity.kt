aa22964c5d9d4f6ea4f82611b7a2e6ba643eb5c9
kim-hyunjin <hyunjin1612@gmail.com>
2024-01-10 22:55:02 +0900

    카카오 로그인정보로 firebase 로그인하기/main activity 변경

--- study/fastcampus/ShareLocation/app/src/main/java/com/github/kimhyunjin/sharelocation/LoginActivity.kt
+++ study/fastcampus/ShareLocation/app/src/main/java/com/github/kimhyunjin/sharelocation/LoginActivity.kt
@@ -1,13 +1,21 @@
 package com.github.kimhyunjin.sharelocation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.github.kimhyunjin.sharelocation.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.auth
+import com.google.firebase.database.database
 import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import com.kakao.sdk.user.model.User
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -18,8 +26,11 @@ class LoginActivity : AppCompatActivity() {
     private val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
         if (error != null) {
             Log.e(TAG, "카카오계정으로 로그인 실패", error)
+            showErrorToast()
+            error.printStackTrace()
         } else if (token != null) {
             Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+            getKakaoAccountInfo()
         }
     }
 
@@ -27,6 +38,9 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "1fee26e3619c1a35f974d497df14516f")
 
         binding.kakaoLoginButton.setOnClickListener {
 
@@ -46,11 +60,99 @@ class LoginActivity : AppCompatActivity() {
                         UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
                     } else if (token != null) {
                         Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                        if (Firebase.auth.currentUser == null) {
+                            // 카카오톡에서 정보를 가져와 파이어베이스 로그인
+                            getKakaoAccountInfo()
+                        } else {
+                            navigateToMapActivity()
+                        }
                     }
                 }
             } else {
                 UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
             }
         }
+    }
+
+    private fun getKakaoAccountInfo() {
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                showErrorToast()
+                error.printStackTrace()
+            } else if (user != null) {
+                Log.i(
+                    "LoginActivity",
+                    "user: ${user.id} / email: ${user.kakaoAccount?.email} / nickname: ${user.kakaoAccount?.profile?.nickname} / profile_photo: ${user.kakaoAccount?.profile?.profileImageUrl}"
+                )
+
+                checkKakaoUserData(user)
+            }
+        }
+    }
+
+    private fun showErrorToast() {
+        Toast.makeText(this, "사용자 로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun checkKakaoUserData(user: User) {
+        val kakaoEmail = user.kakaoAccount?.email.orEmpty()
+
+        if (kakaoEmail.isEmpty()) {
+            // 추가로 이메일을 받는 작업
+            return
+        }
+
+        tryToSignInFirebase(user, kakaoEmail)
+    }
+
+    private fun tryToSignInFirebase(user: User, email: String) {
+        val uid = user.id.toString()
+
+        Firebase.auth.createUserWithEmailAndPassword(email, uid).addOnCompleteListener {
+            if (it.isSuccessful) {
+                updateFirebaseUserInfo(user)
+            } else {
+                showErrorToast()
+            }
+        }.addOnFailureListener { firebaseCreateUserError ->
+            if (firebaseCreateUserError is FirebaseAuthUserCollisionException) {
+                // 이미 가입된 계정 - 로그인 시도
+                signInFirebase(user, email, uid)
+            } else {
+                showErrorToast()
+                firebaseCreateUserError.printStackTrace()
+            }
+        }
+    }
+
+    private fun signInFirebase(user: User, email: String, password: String) {
+        Firebase.auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { authResult ->
+                if (authResult.isSuccessful) {
+                    updateFirebaseUserInfo(user)
+                } else {
+                    showErrorToast()
+                }
+            }.addOnFailureListener { error ->
+                showErrorToast()
+                error.printStackTrace()
+            }
+    }
+
+    private fun updateFirebaseUserInfo(user: User) {
+        val uid = Firebase.auth.currentUser?.uid.orEmpty()
+
+        val userMap = mutableMapOf<String, Any>()
+        userMap["uid"] = uid
+        userMap["name"] = user.kakaoAccount?.profile?.nickname.orEmpty()
+        userMap["profilePhoto"] = user.kakaoAccount?.profile?.profileImageUrl.orEmpty()
+
+        Firebase.database.reference.child("Person").child(uid).updateChildren(userMap)
+
+        navigateToMapActivity()
+    }
+
+    private fun navigateToMapActivity() {
+        startActivity(Intent(this, MapActivity::class.java))
     }
 }
--- study/fastcampus/ShareLocation/app/src/main/java/com/github/kimhyunjin/sharelocation/MapActivity.kt
+++ study/fastcampus/ShareLocation/app/src/main/java/com/github/kimhyunjin/sharelocation/MapActivity.kt
@@ -1,35 +1,23 @@
 package com.github.kimhyunjin.sharelocation
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.github.kimhyunjin.sharelocation.databinding.ActivityMainBinding
+import com.github.kimhyunjin.sharelocation.databinding.ActivityMapBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.kakao.sdk.common.KakaoSdk
-import com.kakao.sdk.common.util.Utility
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMapBinding
     private lateinit var googleMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val keyhash = Utility.getKeyHash(this)
-        Log.i("keyhash", keyhash)
-
-        // Kakao SDK 초기화
-        KakaoSdk.init(this, "1fee26e3619c1a35f974d497df14516f")
-
-//        startActivity(Intent(this, LoginActivity::class.java))
 
         val mapFragment =
             supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
