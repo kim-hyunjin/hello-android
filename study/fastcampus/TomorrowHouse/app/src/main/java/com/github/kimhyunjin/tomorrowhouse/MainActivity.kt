6de3bc393abb4b4df3d6a134743d71ce1acbddfd
kim-hyunjin <hyunjin1612@gmail.com>
2024-01-18 01:01:35 +0900

    버텀 메뉴바 네비게이션 처리

--- study/fastcampus/TomorrowHouse/app/src/main/java/com/github/kimhyunjin/tomorrowhouse/MainActivity.kt
+++ study/fastcampus/TomorrowHouse/app/src/main/java/com/github/kimhyunjin/tomorrowhouse/MainActivity.kt
@@ -3,6 +3,8 @@ package com.github.kimhyunjin.tomorrowhouse
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.github.kimhyunjin.tomorrowhouse.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.Firebase
@@ -16,7 +18,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        /**
+         * 참고 https://developer.android.com/guide/navigation/navigation-ui?hl=ko#bottom_navigation
+         *
+         * 메뉴의 id와 동일한 id를 가진 fragment로 navigation처리해줌
+         */
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
     }
 
 
