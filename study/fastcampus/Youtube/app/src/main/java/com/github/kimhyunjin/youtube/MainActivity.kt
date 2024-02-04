98ee286930897180f9ce0369ecd3facd2f618854
kim-hyunjin <hyunjin1612@gmail.com>
2024-02-04 14:23:33 +0900

    hide상태 적용, 비디오 컨트롤러 수정

--- study/fastcampus/Youtube/app/src/main/java/com/github/kimhyunjin/youtube/MainActivity.kt
+++ study/fastcampus/Youtube/app/src/main/java/com/github/kimhyunjin/youtube/MainActivity.kt
@@ -4,10 +4,12 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.kimhyunjin.youtube.databinding.ActivityMainBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,15 +28,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        initMotionLayout()
+        initVideoRecyclerView()
+
+        initControlButton()
+    }
+
+    private fun initControlButton() {
+        binding.controlButton.setOnClickListener {
+            player?.let {
+                if (it.isPlaying) {
+                    it.pause()
+                } else {
+                    it.play()
+                }
+            }
+        }
+
+        binding.hideButton.setOnClickListener {
+            binding.motionLayout.transitionToState(R.id.hide)
+            player?.pause()
+        }
+    }
+
+    private fun initVideoRecyclerView() {
         videoAdapter = VideoAdapter(context = this) { videoItem ->
             binding.motionLayout.setTransition(R.id.collapse, R.id.expand)
             binding.motionLayout.transitionToEnd()
 
             play(videoItem)
         }
-
-        binding.motionLayout.targetView = binding.videoPlayerContainer
-        binding.motionLayout.jumpToState(R.id.collapse)
 
         binding.videoListRecyclerView.apply {
             layoutManager = LinearLayoutManager(context)
@@ -45,17 +68,70 @@ class MainActivity : AppCompatActivity() {
         videoAdapter.submitList(videoList.videos)
     }
 
-    private fun play(videoItem: VideoItem) {
-        player?.setMediaItem(MediaItem.fromUri(Uri.parse(videoItem.videoUrl)))
-        player?.prepare()
-        player?.play()
+    private fun initMotionLayout() {
+        binding.motionLayout.targetView = binding.videoPlayerContainer
+        // 처음엔 hide상태로 시작
+        binding.motionLayout.jumpToState(R.id.hide)
+        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                binding.playerView.useController = false
+            }
+
+            // 확장된 상태에서만 player 컨트롤러 사용하기
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                binding.playerView.useController = currentId == R.id.expand
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+            }
+
+        })
     }
 
     private fun initExoPlayer() {
         player = ExoPlayer.Builder(this).build().also {
             binding.playerView.player = it
+            binding.playerView.useController = false
+            it.addListener(object : Player.Listener {
+                // player 상태에 따라 컨트롤 버튼 아이콘 바꾸기
+                override fun onIsPlayingChanged(isPlaying: Boolean) {
+                    super.onIsPlayingChanged(isPlaying)
+                    if (isPlaying) {
+                        binding.controlButton.setImageResource(R.drawable.ic_baseline_pause_24)
+                    } else {
+                        binding.controlButton.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+                    }
+                }
+            })
         }
     }
+
+    private fun play(videoItem: VideoItem) {
+        player?.setMediaItem(MediaItem.fromUri(Uri.parse(videoItem.videoUrl)))
+        player?.prepare()
+        player?.play()
+
+        binding.videoTitleTextView.text = videoItem.title
+    }
+
 
     override fun onStart() {
         super.onStart()
