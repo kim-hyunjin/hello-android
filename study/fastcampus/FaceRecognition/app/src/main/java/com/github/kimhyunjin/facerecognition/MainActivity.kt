2afe708c0b3c0b78c0934582083feb37e3667ae1
kim-hyunjin <hyunjin1612@gmail.com>
2024-02-04 18:38:10 +0900

    초기 메인 UI 그리기

--- study/fastcampus/FaceRecognition/app/src/main/java/com/github/kimhyunjin/facerecognition/MainActivity.kt
+++ study/fastcampus/FaceRecognition/app/src/main/java/com/github/kimhyunjin/facerecognition/MainActivity.kt
@@ -3,10 +3,21 @@ package com.github.kimhyunjin.facerecognition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import com.github.kimhyunjin.facerecognition.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.startDetectButton.setOnClickListener {
+            it.isVisible = false
+        }
     }
+
 }
