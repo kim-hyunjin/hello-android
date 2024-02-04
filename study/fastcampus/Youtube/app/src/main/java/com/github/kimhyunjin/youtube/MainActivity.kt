1250760b727e37d233c1657f395d0bcee5b3f51f
kim-hyunjin <hyunjin1612@gmail.com>
2024-02-04 13:46:30 +0900

    exoplayer 적용

--- study/fastcampus/Youtube/app/src/main/java/com/github/kimhyunjin/youtube/MainActivity.kt
+++ study/fastcampus/Youtube/app/src/main/java/com/github/kimhyunjin/youtube/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.github.kimhyunjin.youtube
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.kimhyunjin.youtube.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         readData("video.json", VideoList::class.java) ?: VideoList(emptyList())
     }
 
+    private var player: ExoPlayer? = null
+
     private lateinit var videoAdapter: VideoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         videoAdapter = VideoAdapter(context = this) { videoItem ->
             binding.motionLayout.setTransition(R.id.collapse, R.id.expand)
             binding.motionLayout.transitionToEnd()
+
+            play(videoItem)
         }
         binding.motionLayout.jumpToState(R.id.collapse)
 
@@ -34,5 +41,42 @@ class MainActivity : AppCompatActivity() {
 
         Log.i("videos", videoList.videos.toString())
         videoAdapter.submitList(videoList.videos)
+    }
+
+    private fun play(videoItem: VideoItem) {
+        player?.setMediaItem(MediaItem.fromUri(Uri.parse(videoItem.videoUrl)))
+        player?.prepare()
+        player?.play()
+    }
+
+    private fun initExoPlayer() {
+        player = ExoPlayer.Builder(this).build().also {
+            binding.playerView.player = it
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (player == null) {
+            initExoPlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player?.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (player == null) {
+            initExoPlayer()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player?.release()
     }
 }
