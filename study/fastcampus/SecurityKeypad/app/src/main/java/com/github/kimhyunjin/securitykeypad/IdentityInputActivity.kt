f159d87ffdc7e88c71c762f654e719d7b736ad9c
kim-hyunjin <hyunjin1612@gmail.com>
2024-02-10 22:37:52 +0900

    휴대폰 인증 순차적으로 정보 입력받기

--- study/fastcampus/SecurityKeypad/app/src/main/java/com/github/kimhyunjin/securitykeypad/IdentityInputActivity.kt
+++ study/fastcampus/SecurityKeypad/app/src/main/java/com/github/kimhyunjin/securitykeypad/IdentityInputActivity.kt
@@ -1,8 +1,15 @@
 package com.github.kimhyunjin.securitykeypad
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 import com.github.kimhyunjin.securitykeypad.databinding.ActivityIdentityInputBinding
+import com.github.kimhyunjin.securitykeypad.util.ViewUtil.hideKeyboard
+import com.github.kimhyunjin.securitykeypad.util.ViewUtil.setEditorActionListener
+import com.github.kimhyunjin.securitykeypad.util.ViewUtil.showKeyboard
+import com.github.kimhyunjin.securitykeypad.util.ViewUtil.showKeyboardDelay
 
 class IdentityInputActivity : AppCompatActivity() {
 
@@ -11,6 +18,52 @@ class IdentityInputActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityIdentityInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView()
+        binding.nameEdit.showKeyboardDelay()
+    }
+
+    private fun initView() {
         binding.view = this
+
+        with(binding) {
+            nameEdit.setEditorActionListener(EditorInfo.IME_ACTION_NEXT) {
+                birthdayLayout.isVisible = true
+                birthdayEdit.showKeyboard()
+            }
+
+            birthdayEdit.doAfterTextChanged {
+                if (birthdayEdit.length() > 7) {
+                    genderLayout.isVisible = true
+                    birthdayEdit.hideKeyboard()
+                }
+            }
+
+            genderChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+                if (!telecomLayout.isVisible) {
+                    telecomLayout.isVisible = true
+                }
+            }
+
+            telecomChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+                if (!phoneLayout.isVisible) {
+                    phoneLayout.isVisible = true
+                    phoneEdit.showKeyboard()
+                }
+            }
+
+            phoneEdit.doAfterTextChanged {
+                if (phoneEdit.length() > 10) {
+                    confirmButton.isVisible = true
+                    phoneEdit.hideKeyboard()
+                }
+            }
+
+            phoneEdit.setEditorActionListener(EditorInfo.IME_ACTION_DONE) {
+                if (phoneEdit.length() > 9) {
+                    confirmButton.isVisible = true
+                    phoneEdit.hideKeyboard()
+                }
+            }
+        }
     }
 }
