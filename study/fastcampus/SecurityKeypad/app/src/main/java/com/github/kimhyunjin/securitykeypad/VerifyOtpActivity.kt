355af552301ac1a43de4f8c01b700934b64423c7
kim-hyunjin <hyunjin1612@gmail.com>
2024-02-12 00:26:06 +0900

    otp 인증 UI 타이머 처리

--- study/fastcampus/SecurityKeypad/app/src/main/java/com/github/kimhyunjin/securitykeypad/VerifyOtpActivity.kt
+++ study/fastcampus/SecurityKeypad/app/src/main/java/com/github/kimhyunjin/securitykeypad/VerifyOtpActivity.kt
@@ -1,16 +1,76 @@
 package com.github.kimhyunjin.securitykeypad
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import com.github.kimhyunjin.securitykeypad.databinding.ActivityVerifyOtpBinding
+import com.github.kimhyunjin.securitykeypad.util.ViewUtil.setEditorActionListener
+import com.github.kimhyunjin.securitykeypad.util.ViewUtil.showKeyboardDelay
 
 class VerifyOtpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityVerifyOtpBinding
+
+    private var timer: CountDownTimer? = object : CountDownTimer(3 * 60 * 1000, 1000) {
+        override fun onTick(p0: Long) {
+            val min = p0 / 1000 / 60
+            val sec = p0 / 1000 % 60
+            binding.timerTextView.text = "$min:${String.format("%02d", sec)}"
+        }
+
+        override fun onFinish() {
+            binding.timerTextView.text = ""
+            Toast.makeText(this@VerifyOtpActivity, "입력 제한시간 초과\n다시 시도해주세요.", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVerifyOtpBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.view = this
+        initView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.otpCodeEdit.showKeyboardDelay()
+    }
+
+    override fun onDestroy() {
+        clearTimer()
+        super.onDestroy()
+    }
+
+    private fun initView() {
+        startTimer()
+        with(binding) {
+            otpCodeEdit.doAfterTextChanged {
+                if (otpCodeEdit.length() >= 6) {
+                    stopTimer()
+                }
+            }
+
+            otpCodeEdit.setEditorActionListener(EditorInfo.IME_ACTION_DONE) {
+
+            }
+        }
+    }
+
+    private fun startTimer() {
+        timer?.start()
+    }
+
+    private fun stopTimer() {
+        timer?.cancel()
+    }
+
+    private fun clearTimer() {
+        stopTimer()
+        timer = null
     }
 }
