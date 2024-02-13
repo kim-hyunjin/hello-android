11e2747b217097abb4c40fe3371083d843678845
kim-hyunjin <hyunjin1612@gmail.com>
2024-02-13 21:27:38 +0900

    dependency update / main activity ui

--- study/fastcampus/ArchitecturePattern/app/build.gradle.kts
+++ study/fastcampus/ArchitecturePattern/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -41,6 +45,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("io.coil-kt:coil:2.2.2")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- study/fastcampus/ArchitecturePattern/app/src/main/java/com/github/kimhyunjin/architecturepattern/MainActivity.kt
+++ study/fastcampus/ArchitecturePattern/app/src/main/java/com/github/kimhyunjin/architecturepattern/MainActivity.kt
@@ -2,10 +2,27 @@ package com.github.kimhyunjin.architecturepattern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kimhyunjin.architecturepattern.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+            it.view = this
+        }
+    }
+
+    fun openMvc() {
+    }
+
+    fun openMvp() {
+    }
+
+    fun openMvvm() {
+    }
+
+    fun openMvi() {
     }
 }
