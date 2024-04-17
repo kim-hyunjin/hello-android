52bc074beb976adedd628fdb15506f386d76a095
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-17 11:36:15 +0900

    broadcast test - 권한받기 방법 수정

--- test/overlay/MyWindowManager/app/src/main/java/com/github/kimhyunjin/mywindowmanager/MainActivity.kt
+++ test/overlay/MyWindowManager/app/src/main/java/com/github/kimhyunjin/mywindowmanager/MainActivity.kt
@@ -2,14 +2,14 @@ package com.github.kimhyunjin.mywindowmanager
 
 import android.content.ComponentName
 import android.content.Intent
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.github.kimhyunjin.mywindowmanager.databinding.ActivityMainBinding
@@ -17,6 +17,16 @@ import com.github.kimhyunjin.mywindowmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val overlayPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) {
+        if (Settings.canDrawOverlays(this)) {
+            runOverlay()
+        } else {
+            Toast.makeText(this, "오버레이를 띄우려면 설정이 필요합니다.", Toast.LENGTH_LONG).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -37,30 +47,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!Settings.canDrawOverlays(this)) {
-                val intent = Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:$packageName")
-                )
-                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE)
-            } else {
-                runOverlay()
-            }
-        } else {
+        if (Settings.canDrawOverlays(this)) {
             runOverlay()
+            return;
         }
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
-            if (!Settings.canDrawOverlays(this)) {
-                Toast.makeText(this, "오버레이를 띄우려면 설정이 필요합니다.", Toast.LENGTH_LONG).show()
-            } else {
-                runOverlay()
-            }
-        }
+        val intent = Intent(
+            Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+            "package:$packageName".toUri()
+        )
+        overlayPermissionLauncher.launch(intent)
     }
 
     private fun runOverlay() {
@@ -91,9 +87,5 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("data", "destroyOverlay")
             sendBroadcast(intent)
         }
-    }
-
-    companion object {
-        private const val ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1
     }
 }
