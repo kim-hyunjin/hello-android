3e92d22bf309726ef1f9df92fefd5687329f324f
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-18 14:10:35 +0900

    [feat] 웹뷰에 띄울 테스트용 페이지

---
+++ test/overlay/vue-project/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const messageToAndroid = ref('')
+const messageFromAndroid = ref('')
+onMounted(() => {
+  window.receiveMessageFromAndroid = (message) => {
+    messageFromAndroid.value = message
+  }
+})
+const sendToAndroid = () => {
+  window.Android.send(messageToAndroid.value)
+}
+</script>
+
+<template>
+  <div class="main">
+    <input v-model="messageToAndroid" />
+    <button @click="sendToAndroid">Send Message to android</button>
+    <p>{{ messageFromAndroid }}</p>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+</style>
